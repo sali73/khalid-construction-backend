281637be81bbb07205529948448cbb909cc819a6
sali <sally.elgendy@hotmail.com>
2021-10-27 17:00:22 -0400

    first commit

--- models/video.js
+++ models/video.js
@@ -4,7 +4,7 @@ const { Schema, model } = require("mongoose");
 const videoSchema = Schema({
   id: { type: String, required: false },
   name: { type: String, required: true },
-  time: { type: String, required: false },
+  time:{ type: Date, default: Date.now },
   // video: { type: String, required: false },
 
   video:{ type: String, required: false },
